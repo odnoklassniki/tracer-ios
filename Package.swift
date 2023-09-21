32067119eaeb1137cd73d1ccd40435ba0adb99dd
Dmitry Rybochkin <dmitry.rybochkin@corp.mail.ru>
2023-09-21 14:26:28 +0300

    Добавлена версия 0.1.35

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OKTracer",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.34/OKTracer.xcframework.zip",
-            checksum: "31dc7f956e9a9f8a09ff5057576fc572d661e471bc82c8d355ff5614e01a0702"),
+            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.35/OKTracer.xcframework.zip",
+            checksum: "ecefb6c9aa5b12b3464a481bbd2d30f1930c00570052682b116a476d14476b41"),
         .binaryTarget(
             name: "OKTracerResources",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.34/TracerResources.xcframework.zip",
-            checksum: "0b431b77c19e045778223f832bbf5a954b958e90794e2d02308660a759432418")
+            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.35/TracerResources.xcframework.zip",
+            checksum: "3ba3c3f03dedeb820bdcea0895b18a902c98e00551c9ec8e18e69ce035b4ba68")
     ]
 )
