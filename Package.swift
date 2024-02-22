6fba879fdb5588210153d6994de388e89cf5f1d5
Dave Chupreev <thedave497@gmail.com>
2024-02-22 13:16:46 +0300

    Bump version 0.1.37.2

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OKTracer",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.37/OKTracer.xcframework.zip",
-            checksum: "7f2e46123f9ccf9d1fae539bd6fa52b58bcab46d37db663de40fb9c806ca5de0"),
+            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.37.2/OKTracer.xcframework.zip",
+            checksum: "bef2f3738b00157a6f56b1fabce890f97e1c645582e13b1f4d9c7acbabf72a38"),
         .binaryTarget(
             name: "OKTracerResources",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.37/TracerResources.xcframework.zip",
-            checksum: "8a438f0d5419f45c08325a0d6b22aa3066aa4a76cffcdfec17e3ca0a8b4ad326")
+            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/0.1.37.2/TracerResources.xcframework.zip",
+            checksum: "17bd32432d71f3571cf69abd4310732f0ad4f5c068afc22a67d9f6eae921d5cd")
     ]
 )
