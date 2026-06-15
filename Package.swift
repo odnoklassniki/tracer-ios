e818274b2e24b08865f631f1fb847fcd70067357
Dave Chupreev <thedave497@gmail.com>
2026-06-15 17:31:13 +0300

    Bump version 1.5.0

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OKTracer",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/1.4.1/OKTracer.xcframework.zip",
-            checksum: "266246b7ce3599cd9eabbb745416c163d3705c1bc39be6a56138f6ff33e557e4"),
+            url: "https://nexus.vkteam.ru/repository/raw-tracer-ios-public/OKTracer/1.5.0/OKTracer.xcframework.zip",
+            checksum: "6c0034e374e79004e9c709d15183fc338b7ec50a00241ca7bc58a482f29301c1"),
         .binaryTarget(
             name: "OKTracerResources",
-            url: "https://artifactory-external.vkpartner.ru/artifactory/okdl-ios-tracer-cache/OKTracer/1.4.1/TracerResources.xcframework.zip",
-            checksum: "1a10985cf76c253574b4050618dae4919d6647279bdabab743871b13554d8f7a")
+            url: "https://nexus.vkteam.ru/repository/raw-tracer-ios-public/OKTracer/1.5.0/TracerResources.xcframework.zip",
+            checksum: "549807f03774db9fe6b166fdf216614bc095f4d8fee61a0ed93af05f8e5000fe")
     ]
 )
---
+++ Specs/OKTracer/1.5.0/OKTracer.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |s|
+    s.name                  = 'OKTracer'
+    s.version               = '1.5.0'
+    s.summary               = 'Tracer is a service from OK.Tech for collecting and analyzing errors in mobile applications for iOS and Android'
+    s.homepage              = 'https://apptracer.ru'
+    s.license               = { :type => 'Tracer\'s License Agreement' }
+    s.author                = { 'David Chupreev' => 'david.chupreev@corp.mail.ru' }
+
+    s.xcconfig              = { 'OTHER_LDFLAGS' => ['$(inherited)', '-weak-lswiftDemangle'] }
+    s.compiler_flags        = '-Wno-shorten-64-to-32 -fno-optimize-sibling-calls'
+    s.library               = 'c++'
+    
+    s.source                = { :http => 'https://nexus.vkteam.ru/repository/raw-tracer-ios-public/OKTracer/1.5.0/OKTracer.xcframework.zip' }
+
+    s.resources             = 'OKTracer.bundle'
+    s.vendored_frameworks   = 'OKTracer.xcframework'
+end
